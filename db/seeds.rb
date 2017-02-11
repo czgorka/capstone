e02a4bc9c19dab06e55060a887736939c91fdde8
Cezary Gorka <czgorka@gmail.com>
2017-02-11 23:26:19 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 cities = City.create([{name: 'Baltimore'}])
-states = State.create([{name: 'Maryland'}])
+#states = State.create([{name: 'Maryland'}])
