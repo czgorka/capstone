fdd63f4b37ca5bd20a26ca6ff35578f5cbecc946
Cezary Gorka <czgorka@gmail.com>
2017-02-11 23:15:16 +0100

    routes for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  scope :path => "/api" do
-    resources :cities, only: [:show, :index]
-    resources :states, only: [:show, :index]
-  end
+#  scope :path => "/api" do
+#    resources :cities, only: [:show, :index]
+#    resources :states, only: [:show, :index]
+#  end
 
   get "/ui" => "ui#index"
   get "/ui#" => "ui#index"
