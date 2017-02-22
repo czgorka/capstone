b535950ca5af32a874f553c294c7e405f799baa9
Cezary Gorka <czgorka@gmail.com>
2017-02-22 13:10:04 +0100

    defaut json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   scope :path => "/api" do
-    resources :cities, only: [:show, :index]
+    resources :cities, only: [:show, :index], defaults: {format: :json}
     # resources :states, only: [:show, :index]
   end
 
